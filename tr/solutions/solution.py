7b8fad6ccec28ed417d627362b20e1855b42ac97
Joshua Nelson <jyn514@gmail.com>
2018-09-26 16:06:56 -0400

    add python solution

---
+++ tr/solutions/solution.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+class defaultdict(dict):
+    # if some character c is not present, return that character
+    __missing__ = lambda self, c: c
+
+def normalize_sets(pattern):
+    result = ''
+    i = 0
+    while i < len(pattern):
+        if pattern[i] == '[':
+            start = pattern[i + 1]
+            # assume i + 2 is '-'
+            end = pattern[i + 3]
+            for c in range(ord(start), ord(end) + 1):
+                result += chr(c)
+            i += 4
+        else:
+            result += pattern[i]
+        i += 1
+    return result
+
+def translate(string, in_pattern, out_pattern):
+    d = defaultdict(zip(normalize_sets(in_pattern), normalize_sets(out_pattern)))
+    return ''.join(d[character] for character in string)
+
+def main():
+    input_pattern = input()
+    output_pattern = input()
+    lines = int(input())
+
+    for i in range(lines):
+        print(translate(input(), input_pattern, output_pattern))
+
+if __name__ == "__main__":
+    main()
